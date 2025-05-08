584caadda85e6c54e4945f6b18c8d9d544da770a
darkj0k <rybushkin09@bk.ru>
2025-05-08 20:29:24 +0400

    Fix Retry gRPC 0.3.2

--- backend/pkg/grpcFactory/grpcFactory.go
+++ backend/pkg/grpcFactory/grpcFactory.go
@@ -1,12 +1,14 @@
 package grpcfactory
 
 import (
-	"backend/pkg/config"
-	"backend/pkg/errors"
 	"context"
+	"time"
 
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
+
+	"backend/pkg/config"
+	"backend/pkg/errors"
 )
 
 type GRPCClientConstructor[T any] func(conn grpc.ClientConnInterface) T
@@ -20,8 +22,27 @@ type Client[T any] struct {
 	Cfg        *config.Config
 }
 
+
 func NewClient[T any](address string, logger *zap.Logger, cfg *config.Config, constructor GRPCClientConstructor[T]) (*Client[T], error) {
-	conn, err := grpc.NewClient(address, grpc.WithInsecure())
+	serviceConfig := `{
+		"methodConfig": [{
+		  "name": [{"service": "your.ServiceName"}],
+		  "retryPolicy": {
+			"maxAttempts": 4,
+			"initialBackoff": "0.5s",
+			"maxBackoff": "5s",
+			"backoffMultiplier": 2.0,
+			"retryableStatusCodes": ["UNAVAILABLE"]
+		  }
+		}]
+	}`
+	conn, err := grpc.Dial(
+		address,
+		grpc.WithInsecure(),
+		grpc.WithBlock(),
+		grpc.WithDefaultServiceConfig(serviceConfig),
+		grpc.WithBackoffMaxDelay(5*time.Second),
+	)
 	if err != nil {
 		logger.Error(errors.ErrServiceUnavailable.Message, zap.Error(errors.ErrServiceUnavailable.Err))
 		return nil, errors.ErrServiceUnavailable
